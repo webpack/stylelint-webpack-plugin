0e574515a8f4c2909e74b42d947979c28cb95e4f
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2019-10-27 08:05:00 -0300

    test: refactor

---
+++ test/LintDirtyModulesPlugin.test.js
@@ -0,0 +1,59 @@
+import LintDirtyModulesPlugin from '../src/LintDirtyModulesPlugin';
+import linter from '../src/linter';
+
+jest.mock('../src/linter');
+
+describe('lint dirty modules only', () => {
+  let plugin;
+  let callback;
+
+  beforeAll(() => {
+    callback = jest.fn();
+
+    plugin = new LintDirtyModulesPlugin(null, { files: ['**\\*.s?(c|a)ss'] });
+  });
+
+  beforeEach(() => {
+    linter.mockRestore();
+    callback.mockRestore();
+  });
+
+  it('skips linting on initial run', () => {
+    expect(plugin.isFirstRun).toBe(true);
+    expect(callback).not.toBeCalled();
+
+    plugin.apply({}, callback);
+
+    expect(plugin.isFirstRun).toBe(false);
+    expect(callback).toBeCalledTimes(1);
+  });
+
+  it('linting on change file', () => {
+    const fileTimestamps = new Map([
+      ['foo/changed.scss', 1],
+      ['bar\\changed.scss', 1],
+      ['new-file.scss'],
+    ]);
+
+    plugin.isFirstRun = false;
+    plugin.prevTimestamps = new Map([
+      ['foo/changed.scss', 2],
+      ['bar\\changed.scss', 2],
+    ]);
+    plugin.apply({ fileTimestamps }, callback);
+
+    expect(linter).toBeCalledTimes(1);
+    expect(callback).not.toBeCalled();
+  });
+
+  it('not linter if files are not changed', () => {
+    const fileTimestamps = new Map([['not-changed.scss', 1]]);
+
+    plugin.isFirstRun = false;
+    plugin.prevTimestamps = fileTimestamps;
+    plugin.apply({ fileTimestamps }, callback);
+
+    expect(linter).not.toBeCalled();
+    expect(callback).toBeCalledTimes(1);
+  });
+});
--- test/lint-dirty-modules-only.test.js
+++ test/lint-dirty-modules-only.test.js
@@ -1,26 +1,6 @@
-import LintDirtyModulesPlugin from '../src/LintDirtyModulesPlugin';
-import linter from '../src/linter';
-
 import pack from './utils/pack';
 
-jest.mock('../src/linter');
-
 describe('lint dirty modules only', () => {
-  let plugin;
-  let callback;
-
-  beforeAll(() => {
-    callback = jest.fn();
-
-    plugin = new LintDirtyModulesPlugin(null, { files: ['**\\*.s?(c|a)ss'] });
-    plugin.isFirstRun = false;
-  });
-
-  beforeEach(() => {
-    linter.mockRestore();
-    callback.mockRestore();
-  });
-
   it('skips linting on initial run', (done) => {
     const compiler = pack('error', { lintDirtyModulesOnly: true });
 
@@ -29,32 +9,5 @@ describe('lint dirty modules only', () => {
       expect(stats.hasErrors()).toBe(false);
       done();
     });
-  });
-
-  it('linting on change file', () => {
-    const fileTimestamps = new Map([
-      ['foo/changed.scss', 1],
-      ['bar\\changed.scss', 1],
-      ['new-file.scss'],
-    ]);
-
-    plugin.prevTimestamps = new Map([
-      ['foo/changed.scss', 2],
-      ['bar\\changed.scss', 2],
-    ]);
-    plugin.apply({ fileTimestamps }, callback);
-
-    expect(linter).toBeCalledTimes(1);
-    expect(callback).not.toBeCalled();
-  });
-
-  it('not linter if files are not changed', () => {
-    const fileTimestamps = new Map([['not-changed.scss', 1]]);
-
-    plugin.prevTimestamps = fileTimestamps;
-    plugin.apply({ fileTimestamps }, callback);
-
-    expect(linter).not.toBeCalled();
-    expect(callback).toBeCalledTimes(1);
   });
 });
