734a1aa863aaa324d0ab6c5024f4da757671f8db
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2020-05-04 16:35:37 -0300

    feat: support typescript

--- src/StylelintError.js
+++ src/StylelintError.js
@@ -1,11 +1,23 @@
+/** @typedef {import('stylelint').LintResult} LintResult */
+/** @typedef {import('./getOptions').Options} Options */
+
 export default class StylelintError extends Error {
+  /**
+   * @param {Partial<string>} messages
+   */
   constructor(messages) {
     super(messages);
     this.name = 'StylelintError';
-    this.stack = false;
+    this.stack = '';
   }
 
+  /**
+   * @param {Options} options
+   * @param {Array<LintResult>} messages
+   * @returns {StylelintError}
+   */
   static format({ formatter }, messages) {
+    // @ts-ignore
     return new StylelintError(formatter(messages));
   }
 }
--- src/getOptions.js
+++ src/getOptions.js
@@ -2,6 +2,26 @@ import validateOptions from 'schema-utils';
 
 import schema from './options.json';
 
+/** @typedef {import("stylelint")} stylelint */
+
+/**
+ * @typedef {Object} Options
+ * @property {string=} context
+ * @property {boolean=} emitError
+ * @property {boolean=} emitWarning
+ * @property {boolean=} failOnError
+ * @property {boolean=} failOnWarning
+ * @property {Array<string> | string} files
+ * @property {Function | string} formatter
+ * @property {boolean=} lintDirtyModulesOnly
+ * @property {boolean=} quiet
+ * @property {string} stylelintPath
+ */
+
+/**
+ * @param {Partial<Options>} pluginOptions
+ * @returns {Options}
+ */
 export default function getOptions(pluginOptions) {
   const options = {
     files: '**/*.s?(c|a)ss',
@@ -10,6 +30,7 @@ export default function getOptions(pluginOptions) {
     ...pluginOptions,
   };
 
+  // @ts-ignore
   validateOptions(schema, options, {
     name: 'Stylelint Webpack Plugin',
     baseDataPath: 'options',
@@ -23,6 +44,11 @@ export default function getOptions(pluginOptions) {
   return options;
 }
 
+/**
+ * @param {stylelint} stylelint
+ * @param {Function | string} formatter
+ * @returns {Function}
+ */
 function getFormatter({ formatters }, formatter) {
   if (typeof formatter === 'function') {
     return formatter;
@@ -31,8 +57,10 @@ function getFormatter({ formatters }, formatter) {
   // Try to get oficial formatter
   if (
     typeof formatter === 'string' &&
+    // @ts-ignore
     typeof formatters[formatter] === 'function'
   ) {
+    // @ts-ignore
     return formatters[formatter];
   }
 
