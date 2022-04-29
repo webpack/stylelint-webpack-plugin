2f4b384a6829f2774218e3865a356dc1f3161c25
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-04-29 14:57:24 -0300

    feat: add stylelint prefix to CLI output for better debugging

--- src/StylelintError.js
+++ src/StylelintError.js
@@ -3,7 +3,7 @@ class StylelintError extends Error {
    * @param {string=} messages
    */
   constructor(messages) {
-    super(messages);
+    super(`\u001b[1;31m[stylelint]\u001b[1;0m ${messages}`);
     this.name = 'StylelintError';
     this.stack = '';
   }
