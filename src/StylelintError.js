70afe32cf8c991662d0b920c32b68aeb63c7d6bd
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-04-30 11:38:02 -0300

    fix: message

--- src/StylelintError.js
+++ src/StylelintError.js
@@ -3,7 +3,7 @@ class StylelintError extends Error {
    * @param {string=} messages
    */
   constructor(messages) {
-    super(`\u001b[1;31m[stylelint]\u001b[1;0m ${messages}`);
+    super(`[stylelint] ${messages}`);
     this.name = 'StylelintError';
     this.stack = '';
   }
