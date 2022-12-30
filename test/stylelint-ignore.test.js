b74fd9ba97696172e850193488979364df48dd65
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2022-12-30 11:01:23 -0300

    style: formatting

--- test/stylelint-ignore.test.js
+++ test/stylelint-ignore.test.js
@@ -2,7 +2,7 @@ import pack from './utils/pack';
 
 describe('stylelint ignore', () => {
   it('should ignore file', (done) => {
-    const compiler = pack('stylelintignore');
+    const compiler = pack('stylelint-ignore');
 
     compiler.run((err, stats) => {
       expect(stats.hasWarnings()).toBe(false);
